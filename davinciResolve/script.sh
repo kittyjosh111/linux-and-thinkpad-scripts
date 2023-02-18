5f08ddf296ffd157dc6ad4b9ba172f13bbc04b11
joshuashi <joshua.shi@gmail.com>
2023-02-18 11:05:24 -0800

    changes to script

--- davinciResolve/script.sh
+++ davinciResolve/script.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 #script to do converstions for mov, mp4, or mkv to a mov file that davinci resolve (free) can read.
-cd "$1"
-mkdir bin
+#hurr durr inefficient programming time
+
+cd "$1" #allows for user to pass in filepaths that contain the videos
+
+#vars
+formatDate=$(date +'%m-%d-%Y') #make the subfolder name unique by prefixing with formatDate
+search="*.mp4 *.mov *.mkv *.insv" #file extensions we search for. Modify as needed
+
 echo "Script starting, please wait..."
-shopt -s nocaseglob
-for file in *.mp4 *.mov *.mkv
+mkdir output_$formatDate #creates subfolder to house the converted videos and log file
+
+shopt -s nocaseglob #begin ignore capitalization
+total=$(ls $search 2>/dev/null | wc -l) #total videos to process
+
+for file in $search
 do 
-    ffmpeg -y -i "$file" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "bin/${file%.*}.mov" 2> bin/log.log
+    #actual conversion
+    ffmpeg -y -i "$file" -vcodec mjpeg -acodec pcm_s16be -f mov "output_$formatDate/${file%.*}.mov" 2>> output_$formatDate/log.log
 done
-shopt -u nocaseglob
-echo "Script finished. Files can be found in bin/."
+shopt -u nocaseglob #end ignore capitalization
+
+echo "Script finished. Files can be found in output_$formatDate/."
--- pdftopng/pdftopng.sh
+++ pdftopng/pdftopng.sh
@@ -1,15 +1,24 @@
 
 #!/bin/bash
 #script to do conversions from pdf to png, so that they may be imported into Xournal++.
-cd "$1"
-mkdir bin
+#hurr durr inefficient programming time
+
+cd "$1" #allows for user to pass in filepaths that contain the videos
+
+#vars
+formatDate=$(date +'%m-%d-%Y') #make the subfolder name unique by prefixing with formatDate
+search="*.mp4 *.mov *.mkv *.insv" #file extensions we search for. Modify as needed
+
 echo "Script starting, please wait..."
-shopt -s nocaseglob
-for file in *.pdf
+mkdir output_$formatDate #creates subfolder to house the converted videos and log file
+
+shopt -s nocaseglob #begin ignore capitalization
+for file in $search
 do 
+    #actual conversion
     pdftoppm "$file" "$file" -png
     mv "$file"-1.png bin/
 done
-shopt -u nocaseglob
-echo "Script finished. Files can be found in bin/."
-PWD=$(pwd)
+shopt -u nocaseglob #end ignore capitalization
+
+echo "Script finished. Files can be found in output_$formatDate/."
