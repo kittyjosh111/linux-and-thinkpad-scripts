985b1a95731edb29432de48df4c6be7843aa1134
joshuashi <joshua.shi@gmail.com>
2023-03-23 11:50:42 -0700

    play and stop toggle for media stuff

---
+++ XFCEtweaks/xfceGenMon/com.kj111.genmon/playStop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This is a script to turn on and off the media stream playing.
+# RELIES ON PLAYERCTL. Install it from your favorite package manager.
+# In genmon prefs, set period to 0 for instant icon changes.
+
+#genmon variable to add a clicking action
+CLICK="playerctl play-pause"
+echo -e "<iconclick>$CLICK</iconclick>"
+
+#show only if theres actual media playing
+if [ `playerctl status` != Stopped ]; then
+  #logic behind which icon to show depending on if audio is playing or not. If media is indeed playing, display the pause icon. If not, display the play icon.
+  if [ -z $(playerctl status | grep "Playing") ]; then
+    echo -e "<icon>media-playback-playing</icon>"
+  else
+    echo -e "<icon>media-playback-paused</icon>"
+  fi
+else
+  echo ""
+fi
