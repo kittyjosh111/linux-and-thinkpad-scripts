4bc72f6f875e5a964877ca93be138e13cdcbbd99
joshuashi <joshua.shi@gmail.com>
2023-03-22 21:56:38 -0700

    add clicky function to the genmon script

--- xfceGenMonMic/genMonScript.sh
+++ xfceGenMonMic/genMonScript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #In genmon prefs, set period to 0 for instant icon changes.
 
+#genmon variable to add a clicking action
+CLICK="amixer -D pulse set Capture toggle"
+echo -e "<iconclick>$CLICK</iconclick>"
+
+#logic behind which icon to show depending on output of amixer. Awk is used to filter and search for the term off in microphone settings.
 if [ -z $(amixer | awk -F'Capture ' '{print $2}' | grep "off") ]; then
   echo -e "<icon>audio-input-microphone</icon>"
 else
   echo -e "<icon>microphone-sensitivity-muted</icon>"
 fi
+
