a12595708f446937c1a44792e22d509cc8c6136f
joshuashi <joshua.shi@gmail.com>
2023-03-23 11:59:26 -0700

    fixing a logic error in the playPause script

--- XFCEtweaks/xfceGenMon/com.kj111.genmon/playStop.sh
+++ XFCEtweaks/xfceGenMon/com.kj111.genmon/playStop.sh
@@ -3,12 +3,12 @@
 # RELIES ON PLAYERCTL. Install it from your favorite package manager.
 # In genmon prefs, set period to 0 for instant icon changes.
 
-#genmon variable to add a clicking action
-CLICK="playerctl play-pause"
-echo -e "<iconclick>$CLICK</iconclick>"
-
 #show only if theres actual media playing
 if [ `playerctl status` != Stopped ]; then
+
+  CLICK="playerctl play-pause"
+  echo -e "<iconclick>$CLICK</iconclick>"
+
   #logic behind which icon to show depending on if audio is playing or not. If media is indeed playing, display the pause icon. If not, display the play icon.
   if [ -z $(playerctl status | grep "Playing") ]; then
     echo -e "<icon>media-playback-playing</icon>"
@@ -16,5 +16,5 @@ if [ `playerctl status` != Stopped ]; then
     echo -e "<icon>media-playback-paused</icon>"
   fi
 else
-  echo ""
+echo ""  
 fi
