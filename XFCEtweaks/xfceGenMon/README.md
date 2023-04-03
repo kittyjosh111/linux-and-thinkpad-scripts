0a8e71cebabf8e032b19ebf24ed7c0f750646ccc
kittyjosh111 <joshua.shi@gmail.com>
2023-04-03 08:03:02 -0700

    update voltage to fix problems with voltage > 10. Also include note about executor gnome extension

--- XFCEtweaks/xfceGenMon/README.md
+++ XFCEtweaks/xfceGenMon/README.md
@@ -3,3 +3,5 @@ Why does the XFCE microphone thing look so bad? Why isn't there an indicator for
 The directory presented here is meant to house various genmon scripts I wrote to replace features from other DEs that i found useful and not in XFCE. For example, there is a genmon script to basically monitor for whether a microphone is on or off. I missed this feature from GNOME but the builtin panel indicator for XFCE looks terrible. Thus using genmon, we can jank up our own version.
 
 Keep this script somewhere genmon can read. For example, move the directory into you ~/.config/ folder and make genmon read from that. Set genmon peiod to a lower number for a faster icon refresh rate.
+
+If you are on GNOME, you can actually use these scripts with the gnome extension "Executor", which provides similar functionality. You can just copy paste the contents of the script into executor.
--- XFCEtweaks/xfceGenMon/com.kj111.genmon/voltage.sh
+++ XFCEtweaks/xfceGenMon/com.kj111.genmon/voltage.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# This is a script to monitor if the caps lock is on or off. Think about GNOME extensions.The icon changes when the caps lock is activated or not.
+# This is a script to monitor the current voltage drawn by the device. There was once a GNOME extension that could do this, but went unmaintained.
 # In genmon prefs, set period to 0 for instant icon changes.
 
 length=1
 
-#logic behind which icon to show depending on status of caps lock. sed is used to filter and search for the term caps in xset output
-let "len=$length + 1"
-let "del=$len + 1"
-numerator=$(cat /sys/class/power_supply/BAT0/power_now | cut -c 1-$len)
-let "denominator=10**$length"
-echo -e " -"$(echo "$numerator/$denominator" | bc -l | cut -c 1-$del)"V "
+#logic behind how to calculate voltage draw
+numerator=$(cat /sys/class/power_supply/BAT0/power_now)
+let "denominator=10**6"
+voltage=$(echo "scale=$length ; $numerator / $denominator" | bc)
+echo "-"$voltage"V"
