0da860643f20d50ce846c10e91952a724beb36df
herstalmakrov4219 <joshua.shi@gmail.com>
2025-09-24 17:18:26 -0700

    well put in some font size config

--- desktopEnvironments/gnome/argos/0-thinkvantage.3s.sh
+++ desktopEnvironments/gnome/argos/0-thinkvantage.3s.sh
@@ -1,6 +1,20 @@
-#for argos (https://github.com/p-e-w/argos)
+#!/bin/bash
+# This is a script to monitor a thinkpad running fedora
+# Link to Argos: https://github.com/p-e-w/argos
+
+###############
+## Variables ##
+###############
+
+# The font sizes for the display in Argos' menu.
+argos_font=12
+
+######################################################################
+## DO NOT MODIFY BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING ##
+######################################################################
+
 thinkvantage
 echo "---"
 if [ "$ARGOS_MENU_OPEN" == "true" ]; then
-  echo "$(thinkvantage -v) | font=monospace"
+  echo "$(thinkvantage -v) | font=monospace size=$argos_font"
 fi
